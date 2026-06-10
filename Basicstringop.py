d867c7b8f9063ef59ade71e31a01cd8b97003340
bhaluk-08 <bhaluklatchumi1807@gmail.com>
2026-06-10 09:12:21 +0530

    Add basic string operations in Basicstringop.py

---
+++ Basicstringop.py
@@ -0,0 +1,7 @@
+s = input("Enter a string: ")
+
+print("Upper Case :", s.upper())
+print("Lower Case :", s.lower())
+print("Count of 'a' :", s.count('a'))
+print("Length :", len(s))
+print("Replace :", s.replace("a", "@"))
